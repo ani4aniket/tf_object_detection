67899a82fa45677f2bd1f09422ddcb291055fb49
Aniket Kumar <aniketkumar049@gmail.com>
2019-10-15 16:59:55 +0530

    initial commit

---
+++ build/precache-manifest.ac8f25267836838636b0d073b78a5843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb333f32027d69170439",
+    "url": "/static/js/main.bb333f32.chunk.js"
+  },
+  {
+    "revision": "1fdb710920e9480ebca2",
+    "url": "/static/js/1.1fdb7109.chunk.js"
+  },
+  {
+    "revision": "bb333f32027d69170439",
+    "url": "/static/css/main.bcdeca6e.chunk.css"
+  },
+  {
+    "revision": "b043ee6797a7f3ae1b4f7873a305e26d",
+    "url": "/index.html"
+  }
+];
